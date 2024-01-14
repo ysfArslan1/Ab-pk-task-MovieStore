32f9b1d35dd7024c4d28ef514b691c78932d75b9
ysfArslan1 <ysf.arslan2017@gmail.com>
2024-01-14 17:50:18 +0300

    Initial

---
+++ Ab-pk-task-MovieStore/Ab-pk-task-MovieStore/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Ab_pk_task_MovieStore
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
